c2450baad7bd7dcf0cd1ff443de3f1a99e5b800e
Saswat <saswatsubhajyotimallick@gmail.com>
2020-07-10 10:23:30 +0530

    several changes

--- DrownDetect.py
+++ DrownDetect.py
@@ -1,5 +1,5 @@
 import cvlib as cv
-from cvlib.object_detection import draw_bbox
+from object_detection import draw_bbox
 import cv2
 import time
 import numpy as np
@@ -17,34 +17,34 @@ while webcam.isOpened():
 
     bbox, label, conf = cv.detect_common_objects(frame)
 
-    if(len(bbox)>0):
-            bbox0 = bbox[0]
-            centre = [0,0]
+    if(len(bbox) > 0):
+        bbox0 = bbox[0]
+        centre = [0, 0]
 
-            centre =[(bbox0[0]+bbox0[2])/2,(bbox0[1]+bbox0[3])/2 ]
+        centre = [(bbox0[0]+bbox0[2])/2, (bbox0[1]+bbox0[3])/2]
 
-            hmov = abs(centre[0]-centre0[0])
-            vmov = abs(centre[1]-centre0[1])
+        hmov = abs(centre[0]-centre0[0])
+        vmov = abs(centre[1]-centre0[1])
 
-            x=time.time()
+        x = time.time()
 
-            threshold = 10
+        threshold = 10
 
-            if(hmov>threshold or vmov>threshold):
-                print(x-t0, 's')
-                t0 = time.time()
-                isDrowning = False
-            else:
-                print(x-t0, 's')
-                if((time.time() - t0) > 10):
-                    isDrowning = True
+        if(hmov > threshold or vmov > threshold):
+            print(x-t0, 's')
+            t0 = time.time()
+            isDrowning = False
+        else:
+            print(x-t0, 's')
+            if((time.time() - t0) > 10):
+                isDrowning = True
 
-            # print('bbox: ', bbox, 'centre:', centre, 'centre0:', centre0)
-            print('Drowning Status: ', isDrowning)
+        # print('bbox: ', bbox, 'centre:', centre, 'centre0:', centre0)
+        print('Drowning Status: ', isDrowning)
 
-            centre0 = centre
+        centre0 = centre
 
-    out = draw_bbox(frame, bbox, label, conf,isDrowning)
+    out = draw_bbox(frame, bbox, label, conf, isDrowning)
     cv2.imshow("Real-time object detection", out)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- object_detection.py
+++ object_detection.py
@@ -1,46 +1,32 @@
-#import necessary packages
 import cv2
 import os
 import numpy as np
-from .utils import download_file
+from utils import download_file
 
 initialize = True
 net = None
-dest_dir = os.path.expanduser('~') + os.path.sep + '.cvlib' + os.path.sep + 'object_detection' + os.path.sep + 'yolo' + os.path.sep + 'yolov3'
+dest_dir = os.path.expanduser('~') + os.path.sep + '.cvlib' + os.path.sep + \
+    'object_detection' + os.path.sep + 'yolo' + os.path.sep + 'yolov3'
 classes = None
-#colors are BGR instead of RGB in python
-COLORS = [0,0,255], [255,0,0]
+
 
 def populate_class_labels():
-
-    #we are using a pre existent classifier which is more reliable and more efficient than one
-    #we could make using only a laptop
-    #The classifier should be downloaded automatically when you run this script
     class_file_name = 'yolov3_classes.txt'
     class_file_abs_path = dest_dir + os.path.sep + class_file_name
-    url = 'https://github.com/saswat0/LifeGuard/raw/master/yolov3.txt'
-    if not os.path.exists(class_file_abs_path):
-        download_file(url=url, file_name=class_file_name, dest_dir=dest_dir)
+
     f = open(class_file_abs_path, 'r')
     classes = [line.strip() for line in f.readlines()]
-
     return classes
 
 
 def get_output_layers(net):
-
-    #the number of output layers in a neural network is the number of possible
-    #things the network can detect, such as a person, a dog, a tie, a phone...
     layer_names = net.getLayerNames()
-
-    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
+    output_layers = [layer_names[i[0] - 1]
+                     for i in net.getUnconnectedOutLayers()]
     return output_layers
 
 
 def draw_bbox(img, bbox, labels, confidence, Drowning, write_conf=False):
-
-    global COLORS
     global classes
 
     if classes is None:
@@ -48,24 +34,21 @@ def draw_bbox(img, bbox, labels, confidence, Drowning, write_conf=False):
 
     for i, label in enumerate(labels):
 
-        #if the person is drowning, the box will be drawn red instead of blue
         if label == 'person' and Drowning:
-            color = COLORS[0]
+            color = [0, 0, 255]
             label = 'DROWNING'
         else:
-            color = COLORS[1]
+            color = [255, 0, 0]
 
         if write_conf:
             label += ' ' + str(format(confidence[i] * 100, '.2f')) + '%'
 
-
-        #you only need to points (the opposite corners) to draw a rectangle. These points
-        #are stored in the variable bbox
-        cv2.rectangle(img, (bbox[i][0],bbox[i][1]), (bbox[i][2],bbox[i][3]), color, 2)
-
-        cv2.putText(img, label, (bbox[i][0],bbox[i][1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
+        cv2.rectangle(img, (bbox[i][0], bbox[i][1]),
+                      (bbox[i][2], bbox[i][3]), color, 2)
+        cv2.putText(img, label, (bbox[i][0], bbox[i][1]-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
     return img
+
 
 def detect_common_objects(image, confidence=0.5, nms_thresh=0.3):
 
@@ -75,10 +58,10 @@ def detect_common_objects(image, confidence=0.5, nms_thresh=0.3):
     global classes
     global dest_dir
 
-    #all the weights and the neural network algorithm are already preconfigured
-    #as we are using YOLO
+    # all the weights and the neural network algorithm are already preconfigured
+    # as we are using YOLO
 
-    #this part of the script just downloads the YOLO files
+    # this part of the script just downloads the YOLO files
     config_file_name = 'yolov3.cfg'
     config_file_abs_path = dest_dir + os.path.sep + config_file_name
 
@@ -95,8 +78,6 @@ def detect_common_objects(image, confidence=0.5, nms_thresh=0.3):
     if not os.path.exists(weights_file_abs_path):
         download_file(url=url, file_name=weights_file_name, dest_dir=dest_dir)
 
-
-
     global initialize
     global net
 
@@ -105,7 +86,8 @@ def detect_common_objects(image, confidence=0.5, nms_thresh=0.3):
         net = cv2.dnn.readNet(weights_file_abs_path, config_file_abs_path)
         initialize = False
 
-    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+    blob = cv2.dnn.blobFromImage(
+        image, scale, (416, 416), (0, 0, 0), True, crop=False)
 
     net.setInput(blob)
 
@@ -130,7 +112,6 @@ def detect_common_objects(image, confidence=0.5, nms_thresh=0.3):
                 class_ids.append(class_id)
                 confidences.append(float(max_conf))
                 boxes.append([x, y, w, h])
-
 
     indices = cv2.dnn.NMSBoxes(boxes, confidences, confidence, nms_thresh)
 
